dc1674be0f4052a0e4743a3ac3e99fd49f09b246
Diego Silva <silva.pucrs@gmail.com>
2020-07-06 23:39:18 -0300

    fix: problem environment

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -4,3 +4,6 @@
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
 --output ./config/.env.dev ./config/.env.dev.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+--output ./config/.env.prod ./config/.env.prod.gpg
