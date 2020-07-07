37bc94cf9d93ad3961605b6f58386f3a50a2fe85
Diego Silva <silva.pucrs@gmail.com>
2020-07-07 00:03:54 -0300

    fix: problem environment

--- config/setting.js
+++ config/setting.js
@@ -2,8 +2,11 @@ import pkg from 'dotenv';
 const { config } = pkg;
 import { join, resolve } from 'path';
 import { ok } from 'assert'
+const production = "production"
+let env = process.env.NODE_ENV || 'dev'
 
-const env = process.env.NODE_ENV || 'dev'
+env = env === "production" ? "prod" : env;
+
 ok(env === "prod" || env === "dev", `a env ${env} é inválida, ou dev ou prod`);
 const __dirname = resolve();
 const configPath = join(__dirname, './config', `.env.${env}`);
